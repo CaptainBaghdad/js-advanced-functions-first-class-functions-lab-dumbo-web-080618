062e63af0c813626271871efd2dca79a70bb0306
Jason Pitts <juniorjr083@gmail.com>
2018-09-18 21:41:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(){
+  var arr = [];
+  const myFunc = function(drivers){ 
+    arr.push(drivers[0],drivers[1]);
+    return arr;
+    
+  }
+  return myFunc;
+  
+  
+}
